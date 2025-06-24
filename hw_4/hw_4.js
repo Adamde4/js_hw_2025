3ff11a5b0083eccf9cbca94ecd4871bf03354fdb
Adamde4 <deril4081@gmail.com>
2025-06-24 19:56:09 +0300

    - hw_4 part 2

--- hw_4/hw_4.js
+++ hw_4/hw_4.js
@@ -59,18 +59,19 @@
 //     document.write(`<p>${text}</p>`)
 // }
 // creator("qwe qwe qwe")
-// creator("some text")
 
 
 // #hOL6126
 // – створити функцію, яка створює ul з трьома елементами li та виводить його через document.write. Текст li задати через аргумент всім однаковий
 
 // function creator_li_3ul(text) {
-//     document.write('<ul>')
-//     document.write(`<li> ${text} </li>`)
-//     document.write(`<li> ${text} </li>`)
-//     document.write(`<li> ${text} </li>`)
-//     document.write('</ul>')
+//     document.write(`
+//                     <ul>
+//                         <li> ${text} </li>
+//                         <li> ${text} </li>
+//                         <li> ${text} </li>
+//                         </ul>
+//                         `)
 // }
 // creator_li_3ul('Hello')
 
@@ -94,17 +95,80 @@
 // #gEFoxMMO
 // – створити функцію, яка приймає масив примітивних елементів (числа,стрінги,булеві), та будує для них список (ul li) та виводить його через document.write
 
+// function foobar(array) {
+//     document.write(`<ul>`)
+//     for (const arrayElement of array) {
+//         document.write(`<li>${arrayElement}</li>`)
+//     }
+//     document.write(`</ul>`)
+// }
+//
+// let array = [123, 'asd', 'qwe', true, false]
+// foobar(array)
+
+
+
 
 // #bovDJDTIjt
 // – створити функцію, яка приймає масив об’єктів з наступними полями id,name,age , та виводить їх в документ. Для кожного об’єкту окремий блок.
+
+// function foobar(users) {
+//     for (const user of users) {
+//         document.write(`<div>${user.id} ${user.name} ${user.age}</div>`);
+//     }
+// }
+//
+// let users = [
+//     {name: 'vasya', age: 31, id: 1},
+//     {name: 'petya', age: 30, id: 2},
+//     {name: 'kolya', age: 29, id: 3},
+//     {name: 'olya', age: 28, id: 4},
+//     {name: 'max', age: 30, id: 5},
+//     {name: 'anya', age: 31, id: 6},
+//     {name: 'oleg', age: 28, sid: 7},
+//     {name: 'andrey', age: 29, id: 8},
+//     {name: 'masha', age: 30, sid: 9},
+//     {name: 'olya', age: 31, id: 10},
+//     {name: 'max', age: 31, id: 11}
+// ];
+// foobar(users)
+
 
 
 // #pghbnSB
 // – створити функцію яка повертає найменьше число з масиву
 
+// function min_num_array(array) {
+//     let min = array[0];
+//     for (const arrayElement of array) {
+//         if (arrayElement < min) {
+//             min = arrayElement
+//         }
+//     }
+//     console.log(min)
+// }
+//
+// let array_num = [1, 22, 55, 101, 15]
+// min_num_array(array_num)
+
 
 // #EKRNVPM
 // – створити функцію sum(arr), яка приймає масив чисел, сумує значення елементів масиву та повертає його. Приклад sum([1,2,10]) //->13
+
+
+// function sum(arr) {
+//     let count = 0
+//     for (const arrElement of arr) {
+//         count += arrElement
+//     }
+//     return count
+// }
+//
+// let array_number = [1, 2, 3, 5]
+// console.log(sum(array_number))
+
+
+
 
 
 // #kpsbSQCt2Lf
@@ -112,6 +176,26 @@
 // Приклад  swap([11,22,33,44],0,1) //=> [22,11,33,44]
 
 
+// function swap(arr, index1, index2) {
+//     let temp = arr[index1];
+//     arr[index1] = arr[index2];
+//     arr[index2] = temp;
+//     return arr;
+// }
+//
+// console.log(swap([11, 22, 33, 44], 0, 1));
+
+
 // #mkGDenYnNjn.html
 // Написати функцію обміну валюти exchange(sumUAH,currencyValues,exchangeCurrency)
 // Приклад exchange(10000,[{currency:’USD’,value:25},{currency:’EUR’,value:42}],’USD’) // => 400
+
+function exchange(sumUAH,currencyValues,exchangeCurrency) {
+    for (const currency of currencyValues) {
+        if (currency.currency === exchangeCurrency) {
+            return sumUAH / currency.value;
+        }
+    }
+}
+
+console.log(exchange(1000, [{currency: 'USD', value: 25}, {currency: 'EUR', value: 42}], 'USD'));
